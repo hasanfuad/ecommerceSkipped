77879f3f3ea787e8a724d986fb36db69b8d68496
hasanfuad <hasanmdfuad@hotmail.com>
2022-01-16 22:24:16 +0400

    e-commerce site commit again

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,91 @@
+import { Badge } from "@material-ui/core";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 60px;
+  color: green;
+  font-weight: bold;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Input = styled.input`
+  border: none;
+`;
+
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+
+const MenuItems = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+`;
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input />
+            <Search style={{ color: "gray", fontSize: "16px" }} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>Fuad Dev</Logo>
+        </Center>
+        <Right>
+          <MenuItems>REGISTER</MenuItems>
+          <MenuItems>SIGN IN</MenuItems>
+          <MenuItems>
+            <Badge badgeContent={4} color="secondary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItems>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
---
+++ src/data.js
@@ -0,0 +1,24 @@
+import { default as image1, default as image2, default as image3 } from "./images/scene.JPG"
+export const sliderItems = [
+    {
+        id: 1,
+        img: image1,
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE!",
+        bg: "f5fafd"
+    },
+    {
+        id: 2,
+        img: image2,
+        title: "Winter SALE",
+        desc: "DON'T COMPROMISE ON STYLE!",
+        bg: "fbf0f4"
+    },
+    {
+        id: 3,
+        img: image3,
+        title: "Autumn SALE",
+        desc: "DON'T COMPROMISE ON STYLE!",
+        bg: "f5fafd"
+    },
+]
